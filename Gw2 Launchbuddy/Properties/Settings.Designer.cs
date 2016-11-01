b88daa1149e0c2a9d90c33fe14d02b3e1241cd72
TheCheatsrichter <thecheatsrichter@gmx.at>
2016-11-02 00:03:14 +0100

    Added Cinema Videomode. First working prototype

--- Gw2 Launchbuddy/Properties/Settings.Designer.cs
+++ Gw2 Launchbuddy/Properties/Settings.Designer.cs
@@ -118,5 +118,41 @@ namespace Gw2_Launchbuddy.Properties {
                 this["cinema_use"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool cinema_video {
+            get {
+                return ((bool)(this["cinema_video"]));
+            }
+            set {
+                this["cinema_video"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool cinema_slideshow {
+            get {
+                return ((bool)(this["cinema_slideshow"]));
+            }
+            set {
+                this["cinema_slideshow"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string cinema_videopath {
+            get {
+                return ((string)(this["cinema_videopath"]));
+            }
+            set {
+                this["cinema_videopath"] = value;
+            }
+        }
     }
 }
