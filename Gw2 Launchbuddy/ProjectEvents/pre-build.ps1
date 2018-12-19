34d186a8aef5ba76bfa3f2b35a8a3efceca65213
KairuByte <kyle2468@hotmail.com>
2018-12-19 02:36:07 -0600

    * Clean up extra pre-build process * Downgrade Fody to prevent rare problem building * Added start of VirusTotal scan script for automated scans

--- Gw2 Launchbuddy/ProjectEvents/pre-build.ps1
+++ Gw2 Launchbuddy/ProjectEvents/pre-build.ps1
@@ -5,3 +5,8 @@
     [string]$ProjectDir
 )
 trap {"Error: $_"}
+
+# Copy "Update Helper.exe" to the Resources folder, to be compiled into the final LaunchBuddy executable.
+$path = "$SolutionDir\Update Helper\$OutDir\Update Helper.exe"
+$destination = "$ProjectDir\Resources\"
+Copy-Item -Path $path -Destination $destination
