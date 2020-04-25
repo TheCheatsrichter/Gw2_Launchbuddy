2598c7c38d550511f6272a873264c9e9fdc75b67
DeltraEcho <DeltraEcho@gmail.com>
2020-04-25 16:24:02 +0930

    Update Settings.Designer.cs
    Fixing merge errors...

--- Gw2 Launchbuddy/Properties/Settings.Designer.cs
+++ Gw2 Launchbuddy/Properties/Settings.Designer.cs
@@ -12,7 +12,6 @@ namespace Gw2_Launchbuddy.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
