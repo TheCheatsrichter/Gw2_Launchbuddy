20247ca4248b744ed2dab54c82d87d70e3111ef8
TheCheatsrichter <thecheatsrichter@gmx.at>
2021-11-02 21:13:15 +0100

    Cleaner buildversion fetching

--- Gw2 Launchbuddy/ObjectManagers/ApiManager.cs
+++ Gw2 Launchbuddy/ObjectManagers/ApiManager.cs
@@ -15,23 +15,60 @@ namespace Gw2_Launchbuddy.ObjectManagers
         {
             WebClient api = new WebClient();
             Regex filter_officialapi = new Regex(@"\d*\d");
+            int newest_build = int.MinValue;
+
             try
             {
-                ClientBuild = filter_officialapi.Match(api.DownloadString("https://api.guildwars2.com/v2/build")).Value;
-
-                if(ClientBuild == "115267")
-                {
-                    Regex filter_assetserver = new Regex(@"\d+");
-                    ClientBuild = filter_assetserver.Match(api.DownloadString("http://assetcdn.101.arenanetworks.com/latest64/101")).Value;
-                    
-                }
-
+                newest_build = int.Parse(filter_officialapi.Match(api.DownloadString("https://api.guildwars2.com/v2/build")).Value);
                 Online = true;
             }
             catch
             {
                 Online = false;
-                MessageBox.Show("The official Guild Wars 2 API is unreachable or down! Launchbuddy can't make sure that your game client is up to date.\nPlease keep your game manually up to date to avoid crashes!");
+            }
+
+
+            Regex filter_assetserver = new Regex(@"\d+");
+            //Reserverd when more servers are online
+            /*
+            IPAddress[] assetips = Dns.GetHostAddresses("assetcdn.101.ArenaNetworks.com");
+            
+            try
+            {
+                foreach(var assetip in assetips)
+                {
+                    int assetversion= int.Parse(filter_assetserver.Match(api.DownloadString($"http://{assetip.ToString()}/latest64/101")).Value);
+                    if (assetversion> newest_build)
+                    {
+                        newest_build = assetversion;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            */
+
+            try
+            {
+                int assetversion = int.Parse(filter_assetserver.Match(api.DownloadString($"http://assetcdn.101.ArenaNetworks.com/latest64/101")).Value);
+                if(assetversion > newest_build)
+                {
+                    newest_build = assetversion;
+                }
+            }
+            catch
+            {
+
+            }
+
+            if(newest_build > 115267)
+            {
+                ClientBuild = newest_build.ToString();
+            }else
+            {
+                MessageBox.Show("The official Guild Wars 2 API is unreachable or down and the asset servers not reachable! Launchbuddy can't make sure that your game client is up to date.\nPlease keep your game manually up to date to avoid crashes!");
             }
         }
 
